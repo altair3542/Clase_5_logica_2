cdc2d6ba7ac41d57cf155a7f62149706397efe5e
Santiago Mesa <thiagomesa21@gmail.com>
2024-12-06 18:41:44 -0500

    primera parte de la clase

---
+++ ciclos_anidados.py
@@ -0,0 +1,19 @@
+#ciclos anidados...
+
+# es un ciclo que suele contener otros ciclos, es decir, hay uno externo y hay uno interno (esto no implica que solo se puedan anidar 2) se usan para procesar datos de diferentes niveles o dimensiones.
+
+# for papa in range(3):
+#     for zanahoria in range(99):
+#         print(f"i={papa}, j={zanahoria}")
+
+
+#para que se aplica...
+#procesar matrices, generar patrones visuales, resolver problemas jerarquicos como tableros o graficos.
+
+# concepto clave.
+#por cada iteracion del ciclo externo, el ciclo interno se ejecuyta completamente.
+
+
+# Procesamientro de matrices con ciclos anidados.
+
+#Matriz en IT: 
